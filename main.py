9dafb4c83b73f6eaa5a92a1b64838b600e3b2984
Dante-Sal <dantecampuslands@gmail.com>
2025-02-07 08:43:45 -0500

    Base de Datos y Abrir Json

---
+++ main.py
@@ -0,0 +1,12 @@
+import json
+
+def abrirMembersJSON():
+    with open("./bbdd_members.json","r") as openFile:
+        dicFinal=json.load(openFile)
+    return dicFinal
+def guardarMembersJSON(dic):
+    with open("./bbdd_members.json","w") as outFile:
+        json.dump(dic,outFile)
+dicMembers={}
+dicMembers=abrirMembersJSON()
+print(dicMembers)
